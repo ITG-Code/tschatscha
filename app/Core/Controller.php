6ca8f138ac8e73c9f6977155cbc5cea531d1adb8
Hannes Kindströmmer <me@brolaugh.com>
2016-10-05 22:54:57 +0200

    Corrected Controller

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -2,17 +2,16 @@
 
 class Controller
 {
-  protected function model(string $modelName, $args = [])
+  protected function model(string $modelName, array $args = [])
   {
-    require_once '../app/Model/' . ucfirst( $modelName ). '.php';
-    $model =  ucfirst($modelName);
-    $model = new $model($args);
-    var_dump($model);
+    require_once '../app/Model/' . ucfirst($modelName) . '.php';
+    $model = ucfirst($modelName);
+    return new $model($args);
   }
 
   protected function view(string $view, array $data = [])
   {
-      //global $data = $data;
-      require_once '../app/view/' . $view . '.php';
+    $data = (object)$data;
+    require_once '../app/view/' . $view . '.php';
   }
 }
