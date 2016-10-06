5e6fe8a71402835e2e86e7f657b9d2e6099b8ad4
Hannes Kindströmmer <me@brolaugh.com>
2016-10-06 09:41:45 +0200

    Added PHPMailer wrapper

---
+++ app/Config/MailerConfig.template.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class MailerConfig
+{
+  protected static $mailer;
+  protected static function initMailer()
+  {
+    self::$mailer = new PHPMailer;
+    //self::$mailer->SMTPDebug = 2;
+    self::$mailer->isSMTP();
+    self::$mailer->Host = 'smtp.gmail.com';
+    self::$mailer->SMTPAuth = true;
+    self::$mailer->SMTPSecure = 'tls';
+    self::$mailer->Port = 587;
+
+    self::$mailer->Username = 'me@gmail.com';
+    self::$mailer->Password = 'secret';
+    self::$mailer->From = 'noreply@gmail.com';
+  }
+}
