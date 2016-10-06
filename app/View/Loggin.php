9f504471be86246b69a6c4f8ea64bf47f12e79c7
Dennis Johansson <t971016dejo@gmail.com>
2016-10-06 10:35:13 +0200

    reg och log required

--- app/View/Loggin.php
+++ app/View/Loggin.php
@@ -1,4 +1,4 @@
 <form>
-	<input type="text" name="username" placeholder="username">
-	<input type="password" name="password" placeholder="password">
+	<input type="text" name="username" placeholder="username" required>
+	<input type="password" name="password" placeholder="password" required>
 </form>
