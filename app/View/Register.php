59a702013c035837467da6228a4db684e52aced5
Dennis Johansson <t971016dejo@gmail.com>
2016-10-06 10:23:08 +0200

    reg fix

--- app/View/Register.php
+++ app/View/Register.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title></title>
-	</head>
-	<body>
-		<form action="" method="POST">
-			<input type="text" name="username" placeholder="Username">
-			<input type="password" name="password" placeholder="Password">
-			<input type="email" name="email" placeholder="Email">
-			<input type="text" name="alias" placeholder="Alias">
-			<input type="text" name="firstname" placeholder="Firstname">
-			<input type="text" name="surname" placeholder="Surname">
-			<input type="date" name="birthday" placeholder="Birthday">
-			<input type="submit" name="submit">
-		</form>
-	</body>
-</html>
+<form action="" method="POST">
+	<input type="text" name="username" placeholder="Username">
+	<input type="password" name="password" placeholder="Password">
+	<input type="email" name="email" placeholder="Email">
+	<input type="text" name="alias" placeholder="Alias">
+	<input type="text" name="firstname" placeholder="Firstname">
+	<input type="text" name="surname" placeholder="Surname">
+	<input type="date" name="birthday" placeholder="Birthday">
+	<input type="submit" name="submit">
+</form>
