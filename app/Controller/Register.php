e36f7a819e4608af02219dad0db87a4fd447d439
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-10-06 10:15:35 +0200

    post-datan

--- app/Controller/Register.php
+++ app/Controller/Register.php
@@ -4,6 +4,15 @@ class Register extends Controller
 {
   public function send($args = []){
       // Where you send register form to process registration
+  	$username = $_POST['username'];
+  	$password = $_POST['password'];
+  	$email = $_POST['email'];
+  	$alias = $_POST['alias'];
+  	$firstname = $_POST['firstname'];
+  	$surname = $_POST['surname'];
+  	$birthday = $_POST['birthday'];
+
+  	
   }
   public function index(){
     $this->view('view/register');
