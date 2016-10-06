8671ebaa2bff6a9cfa574bb96ca0f7703ee360f1
Hannes Kindströmmer <me@brolaugh.com>
2016-10-06 12:07:56 +0200

    Added activate account function

--- app/Model/User.php
+++ app/Model/User.php
@@ -19,6 +19,21 @@ class User extends Model
     }else
       return false;
   }
+  public function activate(string $token): bool{
+    // Checks if the token is valid
+    $stmt = self::prepare("SELECT * FROM email_confirm WHERE token = ? AND created_at = changed_at AND used = 0");
+    $stmt->bind_param('s',$token);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if(!$result->num_rows >= 1){
+      return false;
+    }
+    $emailConfirm = $result->fetch_object();
+    $userId = $emailConfirm->user_id;
+
+    $stmt = self::prepare("UPDATE email_confirm SET ")
+
+  }
   public static function login(string $username, string $password): bool{
     $stmt = self::prepare('SELECT * FROM user WHERE username = ?');
     $stmt->bind_param('s', $username);
