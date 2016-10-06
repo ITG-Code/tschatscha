fc76b67ced9d4cbdfc051b2df6592d9dbd9355b2
Hannes Kindströmmer <me@brolaugh.com>
2016-10-06 12:06:18 +0200

    Fixed dependecy problem with Model

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,6 +3,11 @@
 
 class User extends Model
 {
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
   public static function exists(int $userid): bool{
     $stmt = self::prepare("SELECT * FROM user WHERE id = ?");
     $stmt->bind_param('i',$userid);
@@ -13,5 +18,18 @@ class User extends Model
       return true;
     }else
       return false;
+  }
+  public static function login(string $username, string $password): bool{
+    $stmt = self::prepare('SELECT * FROM user WHERE username = ?');
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if(!$result->num_rows >= 1){
+      $stmt->close();
+      $result->close();
+      return false;
+    }
+    return password_verify($password, $result->fetch_object()->password);
+
   }
 }
