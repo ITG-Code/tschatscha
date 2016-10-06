4a664d35676a77b6d19c846fac4e1301e08b664b
Hannes Kindströmmer <me@brolaugh.com>
2016-10-06 09:46:59 +0200

    Added user exists function

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,5 +3,15 @@
 
 class User extends Model
 {
-
+  public static function exists(int $userid): bool{
+    $stmt = self::prepare("SELECT * FROM user WHERE id = ?");
+    $stmt->bind_param('i',$userid);
+    $result = $stmt->get_result();
+    $stmt->close();
+    if($result->num_rows >= 1){
+      $result->close();
+      return true;
+    }else
+      return false;
+  }
 }
