5991109035114fd832e7ff09d562521731b70de3
Dennis Johansson <t971016dejo@gmail.com>
2016-10-06 10:09:01 +0200

    register fil

---
+++ app/View/Register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<form action="" method="POST">
+			<input type="text" name="Username" placeholder="Username">
+			<input type="password" name="Password" placeholder="Password">
+			<input type="email" name="Email" placeholder="Email">
+			<input type="text" name="Alias" placeholder="Alias">
+			<input type="text" name="Firstname" placeholder="Firstname">
+			<input type="text" name="Surname" placeholder="Surname">
+			<input type="date" name="Birthday" placeholder="Birthday">
+			<input type="submit" name="Submit">
+		</form>
+	</body>
+</html>
