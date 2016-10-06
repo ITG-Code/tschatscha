e619943ae87599619811ebbb7703311c0f192296
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-10-06 10:53:54 +0200

    d

--- app/Model/User.php
+++ app/Model/User.php
@@ -16,6 +16,24 @@ class User extends Model
   }
 
   public static function create($username,$password,$email,$alias,$firstname,$surname,$birthday){
-    
+    $SQL = "INSERT INTO user (username,password,email,alias,firstname,surname,birthday) VALUES('$username','$password','$email','$alias','$firstname','$surname','$birthday')";
+    $result = self::query($SQL);
+  }
+  public static function emailExist($email){
+    $SQL = "SELECT email FROM user WHERE email = '$email'";
+    $result = self::query($SQL);
+    if($result->num_rows > 0){
+      $row = $result -> fetch_assoc();
+      echo "Success";
+
+    }else{
+      echo "Ett konto finns redan på den här email-adressen.";
+    }
+  }
+  public static function usernameExist($username){
+
+  }
+  public static function passwordCheck($password){
+
   }
 }
