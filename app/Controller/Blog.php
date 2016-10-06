49f8adcd74a624521b53eda5b155a7ba7d317f86
Pontus Erlandsson <wnedpro@live.se>
2016-10-06 15:21:12 +0200

    regex 3.0 reloaded nu med kommentar ommorogndagen

--- app/Controller/Blog.php
+++ app/Controller/Blog.php
@@ -8,6 +8,7 @@ class Blog extends Controller
     if(strlen($urlname) >= 4){
       echo "domännamnet måste vara minst fyra karaktärer långt";
     }
+    //fixa retur grejs eller elseifgrejs.
     if (!preg_match("/^[a-zA-Z0-9].[a-zA-Z0-9-_]+$/",$urlname)){
       $urlErr = "Tillåtna tecken: A-Z,0-9,bindestreck och understreck";
       echo $urlErr;
