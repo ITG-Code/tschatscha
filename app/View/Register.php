6271440b266f0fff6100566cef3caf4037a0c068
Dennis Johansson <t971016dejo@gmail.com>
2016-10-06 10:10:00 +0200

    registrering

--- app/View/Register.php
+++ app/View/Register.php
@@ -5,14 +5,14 @@
 	</head>
 	<body>
 		<form action="" method="POST">
-			<input type="text" name="Username" placeholder="Username">
-			<input type="password" name="Password" placeholder="Password">
-			<input type="email" name="Email" placeholder="Email">
-			<input type="text" name="Alias" placeholder="Alias">
-			<input type="text" name="Firstname" placeholder="Firstname">
-			<input type="text" name="Surname" placeholder="Surname">
-			<input type="date" name="Birthday" placeholder="Birthday">
-			<input type="submit" name="Submit">
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password" placeholder="Password">
+			<input type="email" name="email" placeholder="Email">
+			<input type="text" name="alias" placeholder="Alias">
+			<input type="text" name="firstname" placeholder="Firstname">
+			<input type="text" name="surname" placeholder="Surname">
+			<input type="date" name="birthday" placeholder="Birthday">
+			<input type="submit" name="submit">
 		</form>
 	</body>
 </html>
