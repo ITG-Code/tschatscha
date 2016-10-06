07faaecbf5ca3bd3d1327aa23159afdf3f146a30
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-10-06 11:02:11 +0200

    FUNCTIONE

--- app/Model/User.php
+++ app/Model/User.php
@@ -16,9 +16,15 @@ class User extends Model
   }
 
   public static function create($username,$password,$email,$alias,$firstname,$surname,$birthday){
+    self::emailExist();
+    self::usernameExist();
+    if(self::emailExist()==1 && self::usernameExist()==1){
     $SQL = "INSERT INTO user (username,password,email,alias,firstname,surname,birthday) VALUES('$username','$password','$email','$alias','$firstname','$surname','$birthday')";
     $result = self::query($SQL);
-  }
+   } else{
+      echo "Erororororor";
+   }
+ }
   public static function emailExist($email){
     $SQL = "SELECT email FROM user WHERE email = '$email'";
     $result = self::query($SQL);
@@ -31,7 +37,15 @@ class User extends Model
     }
   }
   public static function usernameExist($username){
+     $SQL = "SELECT username FROM user WHERE username = '$username'";
+    $result = self::query($SQL);
+    if($result->num_rows > 0){
+      $row = $result -> fetch_assoc();
+      echo "Success";
 
+    }else{
+      echo "Användarnamnet finns redan.";
+    }
   }
   public static function passwordCheck($password){
 
