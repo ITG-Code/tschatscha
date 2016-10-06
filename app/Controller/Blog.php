794e8540675cb4eb61cd58ec9a81c705e00dddb8
Pontus Erlandsson <wnedpro@live.se>
2016-10-06 14:57:17 +0200

    skapablog 2.0 med validations

--- app/Controller/Blog.php
+++ app/Controller/Blog.php
@@ -3,12 +3,20 @@ class Blog extends Controller
 {
   public function create()
   {
-    $blogname = strip_tags($_POST['blogname']);
-    $urlname = strip_tags($_POST['urlname']);
-
-    
-    $blogModel = $this->model('BlogModel');
+    $blogname = $_POST['blogname'];
+    $urlname = $_POST['urlname'];
+    if (!preg_match("/^\S[a-öA-Ö\-\_0-9\s]*$/",$blogname)) {
+      $nameErr = "Tillåtna tecken: A-Z,0-9,bindestreck, understreck och mellanslag";
+      echo $nameErr;
+    }
+    else if (!preg_match("/^[a-zA-Z\-\_0-9]*$/",$urlname)){
+      $urlErr = "Tillåtna tecken: A-Z,0-9,bindestreck och understreck";
+      echo $urlErr;
+    }
+    else{
+    $blogModel = $this->model('Blog');
     $blogModel->createBlog($blogname,$urlname);
+    }
   }
   public function createForm()
   {
--- app/View/createBlog.php
+++ app/View/createBlog.php
@@ -1,5 +1,5 @@
 <form id="createBlog" action="/blog/create" method="post" enctype ="multipart/form-data">
-  Bloggnamn: <input type="text" name="blogname" required>
-  URL: <input type="text" name="urlname" required>
-  <input type="submit" name="submitblog" value="Skapa">
+  <p>Bloggnamn: </br><input type="text" name="blogname" required> </br>
+  URL: </br><input type="text" name="urlname" required></br>
+  <input type="submit" name="submitblog" value="Skapa"></p>
 </form>
