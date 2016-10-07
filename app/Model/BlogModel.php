2ed9c57ccdc6f811257570bc2a039d64420b8150
Pontus Erlandsson <wnedpro@live.se>
2016-10-07 13:53:44 +0200

    Skapa blog version 3.1? med tags och todo klar
    fixat nsfw tagsen när de kryssas i i rutan. Fixat hur det fungerar med
    längd på sträng för urlnamn och bloggnamn.

--- app/Model/BlogModel.php
+++ app/Model/BlogModel.php
@@ -8,12 +8,24 @@ class BlogModel extends Model
     parent::__construct();
   }
 
-  public function createBlog($blogname,$urlname)
+  public function createBlog($blogname,$urlname,$nsfw)
   {
-    $sql = "INSERT INTO blog(name, url_name) VALUES(?,?)";
-    $stmt = $this->prepare($sql);
+    $sqlblog = "INSERT INTO blog(name, url_name) VALUES(?,?)";
+    $stmt = $this->prepare($sqlblog);
     $stmt->bind_param("ss",$blogname,$urlname);
     $stmt->execute();
+    $current_id = $stmt->insert_id;
+    echo $current_id;
+    echo '</br>';
+    echo 'nsfw: ' , $nsfw , ' id=' , $current_id;
+
+
+    if($nsfw==1){
+      $sqlnsfw = "INSERT INTO blog_tag(blog_id,tag_id) VALUES (?,'1')";
+      $stmtnsfw = $this->prepare($sqlnsfw);
+      $stmtnsfw->bind_param("i",$current_id);
+      $stmtnsfw->execute();
+    }
   }
 
 
