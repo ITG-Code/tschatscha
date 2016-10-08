00e192057e323764a052838e076f3c6c6e9a8668
Hannes Kindströmmer <me@brolaugh.com>
2016-10-08 23:36:49 +0200

    Made code pretty

--- app/Controller/Blog.php
+++ app/Controller/Blog.php
@@ -1,37 +1,30 @@
 <?php
+
 class Blog extends Controller
 {
   public function create()
   {
     $blogname = $_POST['blogname'];
     $urlname = $_POST['urlname'];
-    if(isset($_POST['nsfw'])){
-    $nsfw = 1;
-    }
-    else{
-      $nsfw = 0;
-    }
+    $nsfw = (isset($_POST['nsfw'])) ? true : false;
 
-    if(strlen($blogname) <= 3){
-      echo "domännamnet måste vara minst fyra karaktärer långt";
-
+    if(!strlen($blogname) >= 4) {
+      Flasher::addError("Domännamnet måste vara minst fyra karaktärer långt");
     }
-    else
-    {
-      if (!preg_match("/^[a-zA-Z0-9].[a-zA-Z0-9-_]+$/",$urlname)&& strlen($urlname <=3)){
-        $urlErr = "Minst fyra karaktärer. Tillåtna tecken: A-Z ,0-9,bindestreck och understreck";
-        echo $urlErr;
-      }
-      else{
-      $blogModel = $this->model('Blog');
-      $blogModel->createBlog($blogname,$urlname,$nsfw);
-        Redirect::to('/login');
-      }
+    if(!preg_match("/^[a-zA-Z0-9].[a-zA-Z0-9-_]+$/", $urlname) && strlen($urlname <= 3)) {
+      Flasher::addError("Minst fyra karaktärer. Tillåtna tecken: A-Z ,0-9,bindestreck och understreck");
     }
-
+    if(Flasher::errorsExist()) {
+      Redirect::to('/blog/createform');
+    }
+    $blogModel = $this->model('Blog');
+    $blogModel->create($blogname, $urlname, $nsfw);
+    Redirect::to('/dashboard');
 
   }
-  public function createForm()
+
+  public
+  function createForm()
   {
     $this->view('createBlog');
   }
