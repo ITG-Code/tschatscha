3b7ad40ebb82d597cec04e459c8d488f5b374d22
Hannes Kindströmmer <me@brolaugh.com>
2016-10-09 21:42:46 +0200

    Tweaked test

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,12 +1,16 @@
 <?php
 
-
 class AppTest extends PHPUnit_Framework_TestCase
 {
-    public function testParseUrl(){
-      $testUrls = ['/','blog/kappa/','//','äöåöäöåöäöåöä','\\\\\n'];
-      foreach($testUrls as $url){
-        App::parseUrl($url);
-      }
+    public function testParseUrl()
+    {
+        require_once 'app/Core/App.php';
+        $testUrls = ['/', 'blog/kappa/', '//', 'äöåöäöåöäöåöä', '\\\\\n',''];
+        foreach ($testUrls as $url) {
+            $parsedUrl = App::parseUrl($url);
+            if(!is_array($parsedUrl)){
+                throw new Exception('URL not parsed to array');
+            }
+        }
     }
 }
