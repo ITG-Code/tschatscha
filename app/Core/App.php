3de67b103d7632e14dcfcf8385bfe09766f69c72
Hannes Kindströmmer <me@brolaugh.com>
2016-10-09 21:48:17 +0200

    Changed execution dir to project root

--- app/Core/App.php
+++ app/Core/App.php
@@ -10,12 +10,12 @@ class App
   public function __construct()
   {
     $url = $this->parseUrl();
-    if(file_exists('../app/Controller/' . ucfirst($url[0]) . '.php')) {
+    if(file_exists('app/Controller/' . ucfirst($url[0]) . '.php')) {
       $this->controller = $url[0];
       unset($url[0]);
     }
     $this->controller = ucfirst($this->controller);
-    require_once '../app/Controller/' . $this->controller . '.php';
+    require_once 'app/Controller/' . $this->controller . '.php';
     $this->controller = new $this->controller();
 
     if(isset($url[1])) {
--- app/init.php
+++ app/init.php
@@ -4,38 +4,38 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+chdir('..');
 //Adding autoloading for libraries
-require_once '../vendor/autoload.php';
+require_once 'vendor/autoload.php';
 // For stylish stack traces
 \php_error\reportErrors();
 
 // Loading non autoloaded files
-require_once '../app/Core/App.php';
-require_once '../app/Helper/Session.php';
-require_once '../app/Helper/Redirect.php';
-require_once '../app/Helper/UserError.php';
+require_once 'app/Core/App.php';
+require_once 'app/Helper/Session.php';
+require_once 'app/Helper/Redirect.php';
+require_once 'app/Helper/UserError.php';
 
-require_once '../app/Core/Controller.php';
-if(!file_exists('../app/Core/DatabaseConfig.php')) {
-  echo "<pre>/**
+require_once 'app/Core/Controller.php';
+if (!file_exists('app/Core/DatabaseConfig.php')) {
+    echo "<pre>/**
  * Class DatabaseConfig not found
  * Copy app/Core/DatabaseConfig.template.php to app/Core/DatabaseConfig.php in order for the Database class to work
  */</pre>";
-  exit();
+    exit();
 } else
-  require_once '../app/Core/DatabaseConfig.php';
-if(!file_exists('../app/Config/MailerConfig.php')) {
-  echo "<pre>/**
+    require_once 'app/Core/DatabaseConfig.php';
+if (!file_exists('app/Config/MailerConfig.php')) {
+    echo "<pre>/**
  * Class MailerConfig not found
  * Copy app/Config/MailerConfig.template.php to app/Config/MailerConfig.php in order for the Database class to work
  */</pre>";
-  exit();
+    exit();
 } else
-  require_once '../app/Config/MailerConfig.php';
-require_once '../app/Helper/Mailer.php';
+    require_once 'app/Config/MailerConfig.php';
+require_once 'app/Helper/Mailer.php';
 
-require_once '../app/Core/Model.php';
+require_once 'app/Core/Model.php';
 
 Session::start();
 $app = new App;
