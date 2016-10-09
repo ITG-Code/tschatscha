0ee467df4f52689ad054a3c95a17f3a8ea6141fd
Hannes Kindströmmer <me@brolaugh.com>
2016-10-09 23:06:02 +0200

    Add App class test

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -13,4 +13,22 @@ class AppTest extends PHPUnit_Framework_TestCase
             }
         }
     }
+    public function testVisitPages(){
+        require_once 'app/init.php';
+
+        $controllers = scandir('app/Controller');
+        unset($controllers[0]);
+        unset($controllers[1]);
+        $controllers = array_values($controllers);
+        foreach ($controllers as $controller){
+            $controller = explode('.', $controller)[0];
+            require_once "app/Controller/$controller.php";
+            $controllerMethods  = get_class_methods($controller);
+
+            new App("$controller/index");
+            foreach ($controllerMethods as $methodName){
+                new App("$controller/$methodName");
+            }
+        }
+    }
 }
