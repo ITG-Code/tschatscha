547ec580be1ec56fc3a70b32bd4008de5917f6e7
Hannes Kindströmmer <me@brolaugh.com>
2016-10-10 09:30:12 +0200

    Add create blog to dashboard

--- app/View/dashboard/index.php
+++ app/View/dashboard/index.php
@@ -3,3 +3,13 @@ Your are logged in
 <form action="/logout" method="post">
   <input type="submit" name="Loggout" value="logga ut">
 </form>
+
+
+<br/>
+<h4>Skapa blogg</h4>
+<form id="createBlog" action="/blog/create" method="post" enctype="multipart/form-data">
+  <p>Bloggnamn: </br><input type="text" name="blogname" required> </br>
+    URL: </br><input type="text" name="urlname" required></br>
+    NSFW: <input type="checkbox" name="nsfw" value="1"/></br>
+    <input type="submit" name="submitblog" value="Skapa"></p>
+</form>
