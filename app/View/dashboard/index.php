f98fab0736a9c7a76cdf7218fecc20f291b8d82d
Hannes Kindströmmer <me@brolaugh.com>
2016-10-10 10:59:55 +0200

    Fixed dashboard again

--- app/View/dashboard/index.php
+++ app/View/dashboard/index.php
@@ -3,3 +3,11 @@ Your are logged in
 <form action="/logout" method="post">
     <input type="submit" name="Loggout" value="logga ut">
 </form>
+ <br/>
+<h4>Skapa blog</h4>
+<form id="createBlog" action="/blog/create" method="post" enctype="multipart/form-data">
+    Bloggnamn: </br><input type="text" name="blogname" required> </br>
+    URL: </br><input type="text" name="urlname" required></br>
+    NSFW: <input type="checkbox" name="nsfw" value="1"/></br>
+    <input type="submit" name="submitblog" value="Skapa">
+</form>
