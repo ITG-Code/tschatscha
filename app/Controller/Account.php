1e602f8b581c4a93a0b6b00ef8e699a42d4bb8db
Dennis Johansson <t971016dejo@gmail.com>
2016-10-10 14:42:46 +0200

    account settings

--- app/Controller/Account.php
+++ app/Controller/Account.php
@@ -1,8 +1,42 @@
 <?php
 class Account extends Controller
 {
+    public function send($args = []) {
+        $alias = $_POST['alias'];
+        $email = $_POST['email'];
+        $newPassword = $_POST['newPassword'];
+        $confirmPassword = $_POST['confirmPassword'];
+        $oldPassword = $_POST['oldPassword'];
+        // $id = $this->userModel->getLoggedInUserId();
+        if ($this->model('User')->checkInput($oldPassword)) {
+            if (isset($_POST['alias'])) {
+                echo $alias;
+                    // $this->model('User')->
+            }
+            if (isset($_POST['email'])) {
+                echo $email;
+                    // $this->model('User')->
+            }
+            if (isset($_POST['newPassword']) && $newPassword == $confirmPassword) {
+                    echo $newPassword;
+                    // $this->model('User')->
+            }
+            if (isset($_POST['nsfw'])) {
+                echo "nsfw";
+                //är nsfw
+                    // $this->model('User')->
+            } else {
+                echo "sfw";
+                //not nsfw
+            }   
+        }
+    }
     public function index($args = [])
     {
+        if(!$this->userModel ->isLoggedIn())
+       {
+          Redirect::to('/login');
+        }
         $this->view('account/index',[
 
         ]);
--- app/View/account/index.php
+++ app/View/account/index.php
@@ -1,31 +1,31 @@
 <link rel="stylesheet" type="text/css" href="/css/style.css"/>
 <div class="wrapper">
-	<form>
+	<form action="/account/send" method="POST">
 		<div>
 			<table class="form">
 				<tr>
-					<td><label for="">Change alias</label></td>
-					<td><input type="text" name="" id=""></td>
+					<td><label for="alias">Change alias</label></td>
+					<td><input type="text" name="alias" id="alias"></td>
 				</tr>
 				<tr>
-					<td><label for="">Change email</label></td>
-					<td><input type="text" name="" id=""></td>
+					<td><label for="email">Change email</label></td>
+					<td><input type="email" name="email" id="email"></td>
 				</tr>
 				<tr>
-					<td><label for="">Change password</label></td>
-					<td><input type="password" name="" id=""></td>
+					<td><label for="newPassword">Change password</label></td>
+					<td><input type="password" name="newPassword" id="newPassword"></td>
 				</tr>
 				<tr>
-					<td><label for="">Allow anonymous comments?</label></td>
-					<td><input type="checkbox" name="" id=""></td>
+					<td><label for="confirmPassword">Confirm change password</label></td>
+					<td><input type="password" name="confirmPassword" id="confirmPassword"></td>
 				</tr>
 				<tr>
-					<td><label for="">Is your blog NSFW?</label></td>
-					<td><input type="checkbox" name="" id=""></td>
+					<td><label for="nsfw">Is your blog NSFW?</label></td>
+					<td><input type="checkbox" name="nsfw" id="nsfw" value="1"></td>
 				</tr>
 				<tr>
 					<td><label for="password"> Confirmation Password</label></td>
-					<td><input type="password" name="" id="password"></td>
+					<td><input type="password" name="oldPassword" id="password"></td>
 				</tr>
 			</table>
 			<input type="submit" name="submit" value="change">
