f896448bc2fd0064be803d2efc31b3340bd06ffe
Dennis Johansson <t971016dejo@gmail.com>
2016-10-10 13:51:32 +0200

    settings fix css

--- app/View/account/index.php
+++ app/View/account/index.php
@@ -1,12 +1,34 @@
-KONTOINSTÄLLNINGAR
-
-
-Change alias
-Change email
-Change password
-
-Allow anonymous comments?
-NSFW content?
-
-Password check
-
+<link rel="stylesheet" type="text/css" href="/css/style.css"/>
+<div class="wrapper">
+	<form>
+		<div>
+			<table class="form">
+				<tr>
+					<td><label for="">Change alias</label></td>
+					<td><input type="text" name="" id=""></td>
+				</tr>
+				<tr>
+					<td><label for="">Change email</label></td>
+					<td><input type="text" name="" id=""></td>
+				</tr>
+				<tr>
+					<td><label for="">Change password</label></td>
+					<td><input type="password" name="" id=""></td>
+				</tr>
+				<tr>
+					<td><label for="">Allow anonymous comments?</label></td>
+					<td><input type="checkbox" name="" id=""></td>
+				</tr>
+				<tr>
+					<td><label for="">Is your blog NSFW?</label></td>
+					<td><input type="checkbox" name="" id=""></td>
+				</tr>
+				<tr>
+					<td><label for="password"> Confirmation Password</label></td>
+					<td><input type="password" name="" id="password"></td>
+				</tr>
+			</table>
+			<input type="submit" name="submit" value="change">
+		</div>
+	</form>
+</div>
