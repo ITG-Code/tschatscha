725c1246172d20638fb07c1e49aa4f85608398b5
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-10-10 13:06:16 +0200

    balasd

--- app/Controller/Account.php
+++ app/Controller/Account.php
@@ -7,4 +7,13 @@ class Account extends Controller
 
         ]);
     }
+    public function changeSettings(){
+    	$alias = (isset($_POST['alias'])) ? $_POST['alias'] : '';
+    	$email = (isset($_POST['email'])) ? $_POST['email'] : '';
+    	$password = (isset($_POST['password'])) ? $_POST['password'] : '';
+
+    	echo"Det här borde fungera";
+
+
+    }
 }
