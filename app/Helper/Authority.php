548a34c6aefb48416da35b59a15173000ab99e05
Hannes Kindströmmer <me@brolaugh.com>
2016-10-11 09:07:52 +0200

    Add const container

---
+++ app/Helper/Authority.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Authority
+{
+    public const POST_PUBLIC = 0;
+    public const POST_UNLISTED = 1;
+    public const BLOG_MODERATE = 2;
+    public const POST_PRIVATE_VIEW = 3;
+
+
+    public const BLOG_CO_WRITER = 6;
+    public const BLOG_OWNER = 7;
+
+}
