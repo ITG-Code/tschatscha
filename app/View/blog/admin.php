6372d4edd2cb2647bc80d0ef036d7f99ba5bc442
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-10-11 09:36:53 +0200

    fixes

--- app/View/blog/admin.php
+++ app/View/blog/admin.php
@@ -1,8 +1,8 @@
 <form method="post" action="blog/setAuthority">
 	<label> Co-writer</label> </br>
-	<input type="radio" name="cowriter"/>
+	<input type="radio" name="authority"/>
 	<label> Private view </label> </br>
-	<input type="radio" name="privateview"/>
+	<input type="radio" name="authority"/>
 	<label> Moderate </label> </br>
-	<input type="radio" name="moderate"/>
+	<input type="radio" name="authority"/>
 </form> 
