395d254b2d209c19628dcfc69240256ca99d43ba
Dennis Johansson <t971016dejo@gmail.com>
2016-10-12 15:18:45 +0200

    postcheck final

--- app/Controller/Blog.php
+++ app/Controller/Blog.php
@@ -102,7 +102,7 @@ class Blog extends Controller
       $blog_id = $this->model('blog')->getBlogId($blogname);
 
       $auth = $this->model('post')checkAuth($blog_id, $user_id);
-      if ($auth >= 6) {
+      if ($auth < 6) {
           Redirect::to('/'.$blogname);
       }
 //        $args[0] == 'send';
