33fbb915278b94cf42f0455c11de7c77da1fa2a5
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-10-12 13:20:15 +0200

    HJÄLP MIG JAG ÄR VILSE I TROLLSKOGEN

--- app/Model/BlogModel.php
+++ app/Model/BlogModel.php
@@ -91,14 +91,17 @@ ORDER BY name ASC
         $row = $result->fetch_object()->id;
         return $row;
     }
-    public static function chooseBlog(string $blogName) : bool{
-        $stmt = self::prepare("SELECT * FROM user_blog INNER JOIN blog ON user_blog.blog_id = blog.id INNER JOIN user ON user_blog.user_id = user.id WHERE authority = 7");
-        $stmt->bind_param('s', $blogName);
+
+    public static function chooseBlog(string $blogName){
+        $stmt = self::prepare("SELECT name AS blogName FROM blog INNER JOIN user_blog ON blog.id = user_blog.blog_id INNER JOIN user ON user_blog.user_id = user.id WHERE user_id = ? AND authority = 7");
+        $stmt->bind_param("s", $blogName);
         $stmt->execute();
         $result = $stmt->get_result();
-        $stmt->close();
-        return ($result->num_rows >= 1) ? true : false;
-
-    }
+        $returnValue = [];
+            while($row = $result->fetch_object()>=1)
+        {
+            $returnValue[] = $row;
+        }
+        return $returnValue;
 
 }
