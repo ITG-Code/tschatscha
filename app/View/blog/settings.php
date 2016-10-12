f927d73f15a992564bef566abdef7cb775ad3617
Hannes Kindströmmer <me@brolaugh.com>
2016-10-12 15:05:35 +0200

    Fixed search when not searching

--- app/View/blog/settings.php
+++ app/View/blog/settings.php
@@ -1,57 +1,57 @@
-
 Give other users authority
-Choose blog: 
+Choose blog:
 <form method="post" action="/blog/settings">
-	<select name="chooseBlog">
-	<?php
-	echo"<option name='chooseBlog'>";
-	$data->blogpicker;
-	
-	echo"</option>";
-	?> 
-	</select>
-	</form>
+    <select name="chooseBlog">
+        <?php
+        echo "<option name='chooseBlog'>";
+        $data->blogpicker;
 
-	<br/>
-	<form method="post" action="/blog/settings">	
-	<label> Co-writer</label> 
-	<input type="radio" name="authority"/></br>
-	<label> Private view </label> 
-	<input type="radio" name="authority"/></br>
-	<label> Moderate </label> 
-	<input type="radio" name="authority"/></br>
-	<input type="submit" value="Confirm"/>
+        echo "</option>";
+        ?>
+    </select>
+</form>
+
+<br/>
+<form method="post" action="/blog/settings">
+    <label> Co-writer</label>
+    <input type="radio" name="authority"/></br>
+    <label> Private view </label>
+    <input type="radio" name="authority"/></br>
+    <label> Moderate </label>
+    <input type="radio" name="authority"/></br>
+    <input type="submit" value="Confirm"/>
 
 </form>
 
 <form method="post" action="/blog/settings">
-	<label>Sök efter alias</label>
-	<input type="text" name="userQuery"/> <input type="submit" value="Search"/>
-	</form>
-	<div class="searchResult">
-	<?php
-		$data->usersearch;
-
-		echo '<table align="left" cellspacing="5" cellpadding="8">
-					<tr>
-						<td><align="left"><b>Förnamn</b></td>
-						<td><align="left"><b>Alias</b></td>
-						<td><align="left"><b>Efternamn</b></td>
-						<td><align="left"><b>Email</b></td>
-					</tr>'; 
-					echo "<form action='' method='post'>";
-
-		foreach ($data->usersearch as $value) {
-			echo '<tr><td align="left">' .
-				'<input type="checkbox">' . $value->first_name . '</td><td align="left">' .
-				$value->alias . '</td><td align="left">' .
-				$value->sur_name . '</td><td align="left">' .
-				$value->email . '</td><td align="left">';
-			echo '</form>';
-			echo '</tr>';
-				//$value->first_name;
-		}
-
-		echo '</table>';
-	?>
-	</div>
+    <label>Sök efter alias</label>
+    <input type="text" name="userQuery"/> <input type="submit" value="Search"/>
+</form>
+<div class="searchResult">
+    <table align="left" cellspacing="5" cellpadding="8">
+        <tr>
+            <td align="left"><b>Förnamn</b></td>
+            <td align="left"><b>Alias</b></td>
+            <td align="left"><b>Efternamn</b></td>
+            <td align="left"><b>Email</b></td>
+        </tr>
+        <form action='' method='post'>
+            <?php foreach ($data->usersearch as $value) { ?>
+                <tr>
+                    <td align="left">
+                        <input type="checkbox"> <?= $value->first_name ?>
+                    </td>
+                    <td align="left">
+                        <?= $value->alias ?>
+                    </td>
+                    <td align="left">
+                        <?= $value->sur_name ?>
+                    </td>
+                    <td align="left">
+                        <?= $value->email ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </form>
+    </table>
+</div>
