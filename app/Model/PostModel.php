2a36360ce95ba4b1bbe6f9001cf2850af67341b7
Willebus <wonka9800@gmail.com>
2016-10-12 10:01:31 +0200

    Hulp

---
+++ app/Model/PostModel.php
@@ -0,0 +1,26 @@
+<?php
+
+class PostModel extends Model
+{
+    public static function getByName(string $name, int $blogid)
+    {
+
+        $stmt = self::prepare('SELECT * FROM post LEFT JOIN blog ON post.blog_id = blog.id WHERE post.url_title = ? AND post.blog_id = ?');
+        $stmt->bind_param('si', $name,$blogid);
+        $stmt->execute();
+        $posts = $stmt->get_result();
+        $stmt->close();
+        if(!$posts->num_rows >= 1)
+        {
+            return false;
+        }
+        $returnValue = $posts->fetch_object();
+        while($row = $posts->fetch_object())
+        {
+            $stack = array($row);
+            array_push($stack, $row);
+        }
+        $posts->close();
+        return $returnValue;
+    }
+}
