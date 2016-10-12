205f1ed3e82315fc98b50c085cdbd906973ed265
Willebus <wonka9800@gmail.com>
2016-10-12 12:20:51 +0200

    ReCaptcha Validations
    Validations

--- app/View/login/index.php
+++ app/View/login/index.php
@@ -7,7 +7,9 @@
 		<label for="password">Password: </label>
 		<input type="password" name="password" placeholder="password" id="password" required><br/>	
 	</div>
+	<div class="g-recaptcha" data-sitekey="6Lf6aQgUAAAAAMp-uBJa-Fg6m-tGx04qfUyTYdoN"></div>
 	<div>
 		<input type="submit" name="login" value="Login"><br/>	
 	</div>
 </form>
+<script src='https://www.google.com/recaptcha/api.js'></script>
