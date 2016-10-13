e5f363b19ae65d2140a5b39a8a315331b8a89d66
Hannes Kindströmmer <me@brolaugh.com>
2016-10-13 15:10:13 +0200

    Added authority validation

--- app/Helper/Authority.php
+++ app/Helper/Authority.php
@@ -11,5 +11,8 @@ class Authority
 
     public const BLOG_CO_WRITER = 6;
     public const BLOG_OWNER = 7;
+    public static function isValidAuthority($authorityLevel){
+        return in_array($authorityLevel, [0,1,3,6,7]);
+    }
 
 }
