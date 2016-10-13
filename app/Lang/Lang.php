6905e3d43f130393b365f2f9c3df2075802464ab
Pontus Erlandsson <wnedpro@live.se>
2016-10-13 14:38:43 +0200

    2 errors för tags

--- app/Lang/Lang.php
+++ app/Lang/Lang.php
@@ -17,7 +17,9 @@ class Lang
     const FORM_EMAIL_SENT_NO = "No email set";
     const FORM_POST_URL_INVALID_CHARS = "Allowed characters are a-z, A-Z, 0-9 and hyphen(-)";
     const FORM_POST_DATE_INVALID = "Invalid date. Please insert a valid date";
-    const FORM_POST_URL_NOT_UNIQUE = "The Title you specified is not unique for your blog.";
+    const FORM_POST_URL_NOT_UNIQUE = "The Title you specified is not unique for your blog";
+    const FORM_TAGS_EXCEED_CHAR_LIMIT = "Your tags exceed the character limit";
+    const FORM_TAG_EXCEED_CHAR_LIMIT = "One of your tags exceed the character limit(30)";
 
     // Forms are valid but other errors sent
     const WARNING_USERNAME_ALREADY_IN_USE = "That username is already in use";
