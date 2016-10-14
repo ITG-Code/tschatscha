0f37f3de17e0289454981b745f09eb35fd173e96
Dennis Johansson <t971016dejo@gmail.com>
2016-10-14 13:30:53 +0200

    tag 1.2

--- app/View/register/index.php
+++ app/View/register/index.php
@@ -1,67 +1,70 @@
 <link rel="stylesheet" type="text/css" href="/css/style.css"/>
 <form action="/register/send" method="POST">
-	<div class="registerForm">
-	<table class="form">
-		<tr>
-			<td>
-		<label for="Username">Username: </label>
-			</td>
-			<td>
-		<input type="text" name="username" placeholder="Username" id="Username" required>
-			</td>
-		</tr>
-		<tr>
-			<td>
-		<label for="Password">Password: </label>
-			</td>
-			<td>
-		<input type="password" name="password" placeholder="Password" id="Password" required>
-			</td>
-		</tr>
-		<tr>
-			<td>
-		<label for="Email"> E-mail</label>
-			</td>
-			<td>
-		<input type="email" name="email" placeholder="Email" id="Email" required>
-			</td>
-		</tr>
-		<tr>
-			<td>
-		<label for="Alias"> Alias: </label>
-			</td>
-			<td>
-		<input type="text" name="alias" placeholder="Alias" id="Alias" required>
-			</td>
-		</tr>
-		<tr>
-			<td>
-		<label for="Firstname">Firstname: </label>
-			</td>
-			<td>
-		<input type="text" name="firstname" placeholder="Firstname" id="Firstname" required>
-			</td>
-		</tr>
-		<tr>
-			<td>
-		<label for="Surname">Surname: </label>
-			</td>
-			<td>
-		<input type="text" name="surname" placeholder="Surname" id="Surname" required>
-			</td>
-		</tr>
-		<tr>
-			<td>
-		<label for="Date">Date: </label>
-			</td>
-			<td>
-		<input type="date" name="birthday" placeholder="yyyy-mm-dd" min="1899-11-28" max="<?php echo date('Y-m-d'); ?>" id="Date" required>
-			</td>
-		</tr>
-	</table>
-		<div class="g-recaptcha" data-sitekey="6Lf6aQgUAAAAAMp-uBJa-Fg6m-tGx04qfUyTYdoN"></div>
-		<input type="submit" name="submit" value="Register">
-		
-	</div>
+    <div class="registerForm">
+    <table class="form">
+        <tr>
+            <td>
+                <label for="Username">Username: </label>
+            </td>
+            <td>
+                <input type="text" name="username" placeholder="Username" id="Username" required>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="Password">Password: </label>
+            </td>
+            <td>
+                <input type="password" name="password" placeholder="Password" id="Password" required>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="Email"> E-mail</label>
+            </td>
+            <td>
+                <input type="email" name="email" placeholder="Email" id="Email" required>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="Alias"> Alias: </label>
+            </td>
+            <td>
+                <input type="text" name="alias" placeholder="Alias" id="Alias" required>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="Firstname">Firstname: </label>
+            </td>
+            <td>
+                <input type="text" name="firstname" placeholder="Firstname" id="Firstname" required>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="Surname">Surname: </label>
+            </td>
+            <td>
+                <input type="text" name="surname" placeholder="Surname" id="Surname" required>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="Date">Date: </label>
+            </td>
+            <td>
+                <input type="date" name="birthday" placeholder="yyyy-mm-dd" min="1899-11-28" max="<?php echo date('Y-m-d'); ?>" id="Date" required>
+            </td>
+        </tr>
+    </table>
+        <div class="g-recaptcha" data-sitekey="6Lf6aQgUAAAAAMp-uBJa-Fg6m-tGx04qfUyTYdoN"></div>
+        <input type="submit" name="submit" value="Register">
+    </div>
+</form>
+
+<form action="/register/test" method="POST">
+	<input type="submit" name="hej">
 </form>
 <script src='https://www.google.com/recaptcha/api.js'></script>
