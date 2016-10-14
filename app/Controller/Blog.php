503355ecf6a0785f87327fbcd477d50e1abc0bc6
Hannes Kindströmmer <me@brolaugh.com>
2016-10-14 09:04:28 +0200

    Add blog wall and post viewage

--- app/Controller/Blog.php
+++ app/Controller/Blog.php
@@ -7,24 +7,37 @@ class Blog extends Controller
     public function __construct(string $blogName = null)
     {
         parent::__construct();
-        $this->blogName =  (isset($blogName)) ? $blogName :  null;
+        $this->blogName = (isset($blogName)) ? $blogName : null;
     }
 
     public function index($args = [])
     {
-        $this->view('blog/index',[
+        // TODO: Add thing that tells if a post exists or not
 
-            //'postlist' => $this->model('Post')->getByName(),
-              'postlist' => $this->model('Post')->get($this->blogName),
-
-
+        $this->view('blog/index', [
+            'postlist' => $this->model('Post')->get($this->blogName),
         ]);
+
+
     }
-     public function settings($args = [])
+
+    public function post($args = [])
     {
-     if(!$this->userModel ->isLoggedIn())
-       {
-          Redirect::to('/login');
+        if ($args[0] == "compose") {
+            unset($args[0]);
+            $args = $args ? array_values($args) : [];
+            $this->compose($args);
+        } else {
+            $this->view('blog/post/index', [
+                'post' => $this->model('Post')->get($this->blogName, $args[0], 0, 0, false),
+            ]);
+        }
+    }
+
+    public function settings($args = [])
+    {
+        if (!$this->userModel->isLoggedIn()) {
+            Redirect::to('/login');
         }
 
         $search = [];
@@ -35,21 +48,16 @@ class Blog extends Controller
         }
 
         $authorityLevel = [];
-        
-        if(isset($_POST['authority']))
-        {
+
+        if (isset($_POST['authority'])) {
             $setAuthority = $_POST['authority'];
             $userId = $_POST['user_id'];
             $authority = $this->model('Blog')->setAuthority($setAuthority, $this->blogName, $userId);
-            
-            // $blogname = $this->blogName;
-            // $blog_id = $this->model('blog')->getBlogId($blogname);      
         }
 
-        $this->view('blog/settings',[
+        $this->view('blog/settings', [
             'usersearch' => $search,
-            'blogname' => $this->blogName,
-          //'authorityLvl' => $authority,
+            //'authorityLvl' => $authority,
         ]);
 
     }
@@ -57,9 +65,8 @@ class Blog extends Controller
     public function create()
     {
 
-       if(!$this->userModel ->isLoggedIn())
-       {
-          Redirect::to('/login');
+        if (!$this->userModel->isLoggedIn()) {
+            Redirect::to('/login');
         }
         $blogname = (isset($_POST['blogname'])) ? $_POST['blogname'] : '';
         $urlname = (isset($_POST['urlname'])) ? $_POST['urlname'] : '';
@@ -76,34 +83,26 @@ class Blog extends Controller
             Redirect::to('/blog/createform');
         }
         $blogModel = $this->model('Blog');
-        $blogModel->create($blogname, $urlname, $nsfw,$currentUser_id);
+        $blogModel->create($blogname, $urlname, $nsfw, $currentUser_id);
         Redirect::to('/dashboard');
     }
 
 
-    public function compose(/*$args = []*/)
+    public function compose($args = [])
     {
-      if(!$this->userModel->isLoggedIn())
-      {
-        Redirect::to('/login');
-      }
-      $user_id = $this->userModel->getLoggedInUserId();
-      $blogname = $this->blogName;
-      $blog_id = $this->model('blog')->getBlogId($blogname);
-
-      $auth = $this->model('post')->checkAuth($blog_id, $user_id);
-      if ($auth < 6) {
-          Redirect::to('/'.$blogname);
-      }
-//        $args[0] == 'send';
-    // $blogname  = $this->blogName;
-      $this->view('blog/post/index', [
-          'blogname' => $this->blogName
-      ]);
+        if (!$this->userModel->isLoggedIn()) {
+            Redirect::to('/login');
+        }
+        $auth = $this->model('post')->checkAuth($this->model('blog')->getBlogId($this->blogName), $this->userModel->getLoggedInUserId());
+        if ($auth < 6) {
+            Redirect::to('/login');
+        }
+        $this->view('blog/post/compose', [
+        ]);
     }
+
     public function sendPost()
     {
-
         $title = $_POST['Title'];
         $url = $_POST['Url'];
         $publishing_date = $_POST['Date'];
@@ -111,19 +110,14 @@ class Blog extends Controller
         $tags = $_POST['Tags'];
 
 
-
         //kollar så att datumet är korrekt angivet.
         $publishing_date = $this->fixDate($publishing_date);
-        //kollar inloggade användarens id.
-        $user_id = $this->userModel->getLoggedInUserId();
-        //kollar bloggens namn.
-        $blogname = $this->blogName;
         //kollar bloggens id.
-        $blog_id = $this->model('blog')->getBlogId($blogname);
+        $blog_id = $this->model('blog')->getBlogId($this->blogName);
         //Tar högsta history_id och höjer det med 1.
         $history_id = $this->model('post')->getHistoryId($url);
         //kollar så att url är korrekt angiven.
-        $url =$this->fixURL($url,$blogname,$blog_id, $blogname);
+        $url = $this->fixURL($url, $this->blogName, $blog_id, $this->blogName);
 
 
         if (isset($_POST['Anon'])) {
@@ -134,27 +128,27 @@ class Blog extends Controller
         $auth = $_POST['auth'];
         $time = date('Y-m-d H:i');
 
-        $id = $this->model('post')->createPost($title, $url, $user_id, $blog_id, $history_id, $content, $publishing_date, $anon, $auth, $time);
+        $id = $this->model('post')->createPost($title, $url, $this->userModel->getLoggedInUserId(), $blog_id, $history_id, $content, $publishing_date, $anon, $auth, $time);
         //fixar taggar
-        $this->model('tag')->checkTag($tags, true, $id, $blogname);
+        $this->model('tag')->checkTag($tags, true, $id, $this->blogName);
 
-        Redirect::to('/'.$blogname.'/') ;
+        Redirect::to("/$this->blogName/$url");
     }
 
     //indata = titel url och blognamn, utdata = titel url/error, byter ut ' ' mot '-' och kolla efter icketillåtna tecken.
     public function fixURL(string $url, string $blogname, int $blog_id)
     {
-      $url = str_replace(' ', '-', $url);
-      $unique = $this->model('post')->checkURL($url,$blog_id);
-      if($unique == false){
-        UserError::add(Lang::FORM_POST_URL_NOT_UNIQUE);
-        Redirect::to('/'.$blogname.'/compose');
-      }
-      if(!preg_match("/^[a-zA-Z0-9].[a-zA-Z0-9-]+$/", $url)){
-        UserError::add(Lang::FORM_POST_URL_INVALID_CHARS);
-        Redirect::to('/'.$blogname.'/compose');
-      }
-      return $url;
+        $url = str_replace(' ', '-', $url);
+        $unique = $this->model('post')->checkURL($url, $blog_id);
+        if ($unique == false) {
+            UserError::add(Lang::FORM_POST_URL_NOT_UNIQUE);
+            Redirect::to('/' . $blogname . '/compose');
+        }
+        if (!preg_match("/^[a-zA-Z0-9].[a-zA-Z0-9-]+$/", $url)) {
+            UserError::add(Lang::FORM_POST_URL_INVALID_CHARS);
+            Redirect::to('/' . $blogname . '/compose');
+        }
+        return $url;
     }
 
     //indata=datum, utdata=datum -T om det finns, kollar så att datum är korrekt angivet.
@@ -171,9 +165,15 @@ class Blog extends Controller
             return $publishing_date;
         } else {
             //felmedelande här inte någon return
-             UserError::add(Lang::FORM_POST_DATE_INVALID);
-             return date('Y-m-d H:i');
+            UserError::add(Lang::FORM_POST_DATE_INVALID);
+            return date('Y-m-d H:i');
         }
 
+    }
+
+    protected function view(string $view, array $data = [])
+    {
+        $data['blogname'] = $this->blogName;
+        parent::view($view, $data);
     }
 }
