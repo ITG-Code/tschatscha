f9598fb8b36e2325a6cf6d06d00cf492a380885d
Hannes Kindströmmer <me@brolaugh.com>
2016-10-14 13:31:05 +0200

    posts on the blog wall has links to the single post

--- app/View/blog/post/single.php
+++ app/View/blog/post/single.php
@@ -1,8 +1,14 @@
 <article class="well well-sm">
-    <header><h2><?= $post->title ?></h2></header>
+    <header>
+        <?php if ($data->linked_title) { ?>
+            <h2><a href="/<?=$data->blogname?>/post/<?=$post->url_title?>"><?= $post->title ?></a> </h2>
+        <?php } else { ?>
+            <h2><?= $post->title ?></h2>
+        <?php } ?>
+    </header>
     <p><?= $data->parsedown->text($post->content) ?></p>
     <footer>
-        <p>Skriven av: <?=$post->first_name, " \"", $post->alias,"\" ", $post->sur_name ?></p>
+        <p>Skriven av: <?= $post->first_name, " \"", $post->alias, "\" ", $post->sur_name ?></p>
         <p class="">Publicerad: <?= $post->publishing_date ?></p>
     </footer>
 </article>
