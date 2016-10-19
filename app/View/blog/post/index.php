e26414f7e58982cb38688c246cfbbae3ed543334
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-10-19 10:19:49 +0200

    Dele

--- app/View/blog/post/index.php
+++ app/View/blog/post/index.php
@@ -5,8 +5,3 @@ $post = $data->post[0];
 require_once 'app/View/blog/post/single.php';
 ?>
 
-<form name="delete" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/delete" method="post">
-<?= $post->id ?>
-<input type="hidden" name="delete" value="<?= $post->id ?>"/>
-<input type="submit" name="<?= $post->id ?>" value="Ta bort" />
-</form>
