923c6cf8df645da4bf463b231bec56dc47dd0bf6
Hannes Kindströmmer <me@brolaugh.com>
2016-10-25 10:59:18 +0200

    Fixed HTML nesting

--- app/View/blog/post/single.php
+++ app/View/blog/post/single.php
@@ -1,130 +1,62 @@
-<?php if (!$data->linked_title) { ?>
-<br/>
-<?php
-        
-        if($data->loggedin){?>
-        <nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <!-- <a class="navbar-brand" href="#"></a> -->
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-
-    <div class="collapse navbar-collapse" id="navbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="/dashboard">Hem</a></li>
-        <!-- <li><a href="#"></a></li> -->
-      </ul>
- 
-    <ul class="nav navbar-nav navbar-right">
-    <li><a href="/<?= $data->blogname ?>">Tillbaka till  flödet</a></li>
-        <?php
-        if($data->auth >=6){?>
-        <li><a href="/<?php echo $data->blogname; ?>/compose">Skriv nytt inlägg</a></li><li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hantera blogg <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            
-              <?php
-        if($data->auth ==7){?>
-        <li><a href="/<?php echo $data->blogname; ?>/settings">Blogginställningar</a></li>
-        <?php } ?>
-              <li></li>
-
-              
-            </ul>
-          </li>
- <?php } ?>
-
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dina bloggar <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <?php  foreach ($data->bloglist as $value) {if ($value->authority >= 6) { ?>
-              <li><a href="/<?= $value->url_name ?>"><?= $value->name ?></a></li>
-              <?php }} ?>
-              
-            </ul>
-          </li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Annat <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="/account/settings">Kontoinställningar</a></li>
-              <li><a href="/blog/allFollowers">Visa alla följare</a></li>
-              <li><a href="/logout">Logga ut</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
-
-     <?php
-     } }?>
 <?php if ((($data->auth >= $post->visibility) || ($post->visibility == 1 && !$data->linked_title)) && ($data->anon + $post->anonymous_allowance >= 1)) { ?>
     <div class="col-md-12">
-        <article class="well well-sm">
-            <header>
-                <?php if (htmlentities($data->linked_title)) { ?>
-                    <h2><a href="/<?= htmlentities($data->blogname) ?>/post/<?= htmlentities($post->url_title) ?>"><?= htmlentities($post->title) ?></a></h2>
-                <?php } else { ?>
-                    <h2><?= $post->title ?></h2>
-                <?php } ?>
-            </header>
-            <p>
-                <?php
-                $content = $data->parsedown->text($post->content);
-                $content = str_replace("<img", "<img class=\"img-responsive\"", $content);
-                echo $content;
-                ?>
-            </p>
-            <footer>
-                <p style="font-size:10px;">Skriven
-                    av: <?= htmlentities($post->first_name), " \"", htmlentities($post->alias), "\" ", htmlentities($post->sur_name) ?>
-                    | Publicerad: <?= $post->publishing_date ?></p>
-                <div>
-                    <p style="font-size:10px;">Taggar: <?= htmlentities($post->tags) ?></p>
-                </div>
-                <?php if (!$data->linked_title) { ?>
-                <?php if ($data->auth >= Authority::BLOG_CO_WRITER) { ?>
-    <table>
-        <tr>
-            <td>
-                <form name="edit" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/edit" method="post">
-                    <input type="submit" class="btn btn-success" name="<?= $post->id ?>" value="Redigera"/>
-                </form>
-            </td>
-            <td>
-                <form name="delete" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/delete" method="post">
-                    <input type="hidden" name="delete" value="<?= $post->id ?>"/>
-                    <input type="submit" class="btn btn-danger" name="<?= $post->id ?>" value="Ta bort"/>
-                </form>
-            </td>
-        </tr>
-    </table>
-<?php } ?>
-            </footer>
+    <article class="well well-sm">
+    <header>
+        <?php if (htmlentities($data->linked_title)) { ?>
+            <h2>
+                <a href="/<?= htmlentities($data->blogname) ?>/post/<?= htmlentities($post->url_title) ?>"><?= htmlentities($post->title) ?></a>
+            </h2>
+        <?php } else { ?>
+            <h2><?= $post->title ?></h2>
+        <?php } ?>
+    </header>
+    <p>
+        <?php
+        $content = $data->parsedown->text($post->content);
+        $content = str_replace("<img", "<img class=\"img-responsive\"", $content);
+        echo $content;
+        ?>
+    </p>
+    <footer>
+    <p style="font-size:10px;">Skriven
+        av: <?= htmlentities($post->first_name), " \"", htmlentities($post->alias), "\" ", htmlentities($post->sur_name) ?>
+        | Publicerad: <?= $post->publishing_date ?></p>
+    <div>
+        <p style="font-size:10px;">Taggar: <?= htmlentities($post->tags) ?></p>
+    </div>
+    <?php if (!$data->linked_title) { ?>
+        <?php if ($data->auth >= Authority::BLOG_CO_WRITER) { ?>
+            <table>
+                <tr>
+                    <td>
+                        <form name="edit" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/edit"
+                              method="post">
+                            <input type="submit" class="btn btn-success" name="<?= $post->id ?>" value="Redigera"/>
+                        </form>
+                    </td>
+                    <td>
+                        <form name="delete" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/delete"
+                              method="post">
+                            <input type="hidden" name="delete" value="<?= $post->id ?>"/>
+                            <input type="submit" class="btn btn-danger" name="<?= $post->id ?>" value="Ta bort"/>
+                        </form>
+                    </td>
+                </tr>
+            </table>
 
-        </article>
+        <?php } ?>
+
         <?php if (!$data->linked_title) { ?>
 
             <?php foreach ($data->comments as $comment) { ?>
                 <article class="well well-sm">
                     <header>
-                        Skriven av:  <?= htmlentities($comment->session_user) ?>
+                        Skriven av: <?= htmlentities($comment->session_user) ?>
                         </br>
-                        Kommentar: <?= htmlentities($comment->content)?>
+                        Kommentar: <?= htmlentities($comment->content) ?>
                     </header>
                 </article>
             <?php } ?>
-
-
             <article class="well well-sm">
                 <header>
                     <label for="content">Kommentera:</label>
@@ -138,22 +70,27 @@
                 </header>
             </article>
         <?php } ?>
+
+    <?php } ?>
+    </footer>
+    </article>
     </div>
-<?php } ?>
-<?php if ($data->auth >= Authority::BLOG_CO_WRITER) { ?>
-    <table>
-        <tr>
-            <td>
-                <form name="delete" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/delete" method="post">
-                    <input type="hidden" name="delete" value="<?= $post->id ?>"/>
-                    <input type="submit" name="<?= $post->id ?>" value="Ta bort"/>
-                </form>
-            </td>
-            <td>
-                <form name="edit" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/edit" method="post">
-                    <input type="submit" name="<?= $post->id ?>" value="redigera"/>
-                </form>
-            </td>
-        </tr>
-    </table>
-<?php }} ?>
+    <?php if ($data->auth >= Authority::BLOG_CO_WRITER) { ?>
+        <table>
+            <tr>
+                <td>
+                    <form name="delete" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/delete"
+                          method="post">
+                        <input type="hidden" name="delete" value="<?= $post->id ?>"/>
+                        <input type="submit" name="<?= $post->id ?>" value="Ta bort"/>
+                    </form>
+                </td>
+                <td>
+                    <form name="edit" action="/<?= $data->blogname ?>/post/<?= $post->url_title ?>/edit" method="post">
+                        <input type="submit" name="<?= $post->id ?>" value="redigera"/>
+                    </form>
+                </td>
+            </tr>
+        </table>
+    <?php }
+} ?>
